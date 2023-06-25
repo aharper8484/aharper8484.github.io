402a6858cbb0ad379c9e4f999b9eae3f35241fb8
aharper8484 <aharper8484@gmail.com>
2023-06-25 14:02:20 +0100

    Added React Markdown
    Enable react markdown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <ReactMarkdown>TEST for Markdown</ReactMarkdown>
   </React.StrictMode>
 );
 
