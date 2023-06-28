b35bf1ea1bfd41363a481790cd303eb23f0dd5ff
aharper8484 <aharper8484@gmail.com>
2023-06-28 16:51:36 +0100

    Small bug fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Post from "./Post";
+import App from "./components/App";
+import Post from "./components/Post";
 import reportWebVitals from "./reportWebVitals";
 // import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
