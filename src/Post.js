3305199613b652f0fcc986e8aefd9a909641ca25
aharper8484 <aharper8484@gmail.com>
2023-06-28 16:44:45 +0100

    Added additional formatting
    Capable of line breaks and importing markdown string

--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,19 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import remarkGfm from "remark-gfm";
 import "./App.css";
 
+const postContent = `
+# Blog - First Post
+This is an example of my first ever blog \n\n post to appear in my React based blog
+
+## This is a H2
+
+And
+
+### H3 header is here`;
+
 function Post() {
-  return (
-    <div className="Post">
-      <ReactMarkdown>### This is the title</ReactMarkdown>
-      <ReactMarkdown>
-        this should be a paragraph on multiple lines
-      </ReactMarkdown>
-      <ReactMarkdown>## Another header</ReactMarkdown>
-      <ReactMarkdown>
-        And another long paragraph where I don't know where it starts and ends
-      </ReactMarkdown>
-    </div>
-  );
+  return <ReactMarkdown children={postContent} remarkPlugins={[remarkGfm]} />;
 }
 
 export default Post;
