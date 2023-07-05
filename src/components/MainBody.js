f279ad713394d9b17158308694432769e2c79e93
aharper8484 <aharper8484@gmail.com>
2023-07-05 12:38:24 +0100

    Added 6 box grid
    CSS added to justify grid

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -2,8 +2,27 @@ import "./MainBody.css";
 
 function MainBody() {
   return (
-    <div className="mainBodyGrid">
-      <h1>Main Body goes here</h1>
+    <div className="mainContainer">
+      <div className="mainGrid">
+        <div className="itemContainer">
+          <h1>One</h1>
+        </div>
+        <div className="itemContainer">
+          <h1>Two</h1>
+        </div>
+        <div className="itemContainer">
+          <h1>Three</h1>
+        </div>
+        <div className="itemContainer">
+          <h1>Four</h1>
+        </div>
+        <div className="itemContainer">
+          <h1>Five</h1>
+        </div>
+        <div className="itemContainer">
+          <h1>Six</h1>
+        </div>
+      </div>
     </div>
   );
 }
