3ecb517cff23fac44c82d272a09c66fd71795e71
aharper8484 <aharper8484@gmail.com>
2023-07-25 10:58:27 +0100

    timeline
    tinkering with timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,161 +1,56 @@
 import "../components/Timeline.css";
 
 function Timeline() {
-  const items = document.querySelectorAll(".timeline-section li");
-  function isElementInViewport(el) {
-    let rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function slideIn() {
-    for (let i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("slide-in");
-      } else {
-        items[i].classList.remove("slide-in");
-      }
-    }
-  }
-
-  window.addEventListener("load", slideIn);
-  window.addEventListener("scroll", slideIn);
-  window.addEventListener("resize", slideIn);
+  const accentColour1 = "#41516C";
 
   return (
-    <main>
-      <section className="intro-textTL">
-        <div className="containerTL">
-          <h1>Tutorial Timeline</h1>
-          <p>A timeline of my accomplishments.</p>
-        </div>
-      </section>
-
-      <section className="timeline-sectionTL">
-        <uL>
-          <li>
-            <div>
-              <time>2001</time>Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2002</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2004</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2006</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2008</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2010</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2012</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2014</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2016</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2019</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2020</time> This is a picture of my first hackathon (Add an
-              image of your choice).
-              <img src="" alt="" />
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2021</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <time>2022</time> Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Incidunt nostrum quidem eius repudiandae nam,
-              nemo fuga architecto possimus id at rem beatae consequuntur dolor
-              commodi.
-            </div>
-          </li>
-        </uL>
-      </section>
-    </main>
+    <div>
+      <h1>UL timeline cards</h1>
+      <ul>
+        <li style={{ background: `#41516C` }}>
+          <div className="dateTL">2002</div>
+          <div className="titleTL">Title 1</div>
+          <div className="descrTL">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
+            itaque hic quibusdam fugiat est numquam harum, accusamus suscipit
+            consequatur laboriosam!
+          </div>
+        </li>
+        <li style={{ background: `#41516C` }}>
+          <div className="dateTL">2007</div>
+          <div className="titleTL">Title 2</div>
+          <div className="descrTL">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+            adipisci nobis nostrum vero nihil veniam.
+          </div>
+        </li>
+        <li style={{ background: `#41516C` }}>
+          <div className="dateTL">2012</div>
+          <div className="titleTL">Title 3</div>
+          <div className="descrTL">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga minima
+            consequuntur soluta placeat iure totam commodi repellendus ea
+            delectus, libero fugit quod reprehenderit, sequi quo, et dolorum
+            saepe nulla hic.
+          </div>
+        </li>
+        <li style={{ background: `#41516C` }}>
+          <div className="dateTL">2017</div>
+          <div className="titleTL">Title 4</div>
+          <div className="descrTL">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit,
+            cumque.
+          </div>
+        </li>
+        <li style={{ background: `#41516C` }}>
+          <div className="dateTL">2022</div>
+          <div className="titleTL">Title 5</div>
+          <div className="descrTL">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, non.
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
 
