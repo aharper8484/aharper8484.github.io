2f85417c099963fe219d4d5d0bb3d097f18f9687
aharper8484 <aharper8484@gmail.com>
2023-07-28 14:09:40 +0100

    update css
    x

--- src/components/ProfileImage 2.js
+++
@@ -1,8 +0,0 @@
-// import profileImage from "../assets/profile-photo.jpg";
-import profileSketch from "../assets/profileSketch.png";
-
-function ProfileImage() {
-  return <img src={profileSketch} alt="" id="profileImage"></img>;
-}
-
-export default ProfileImage;
